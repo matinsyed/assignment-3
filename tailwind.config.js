7676f2251291850aad0a2b907b5632d05bc0a86e
matinsyed <coder.matin@gmail.com>
2025-02-06 01:21:18 +0600

    navbar added

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+      colors: {
+        'accent-color': '#FFDC26',
+        'secondary-color': '#FFF9F4',
+        'primary-color': '#FFFFFF',
+      },
+    },
+  }
